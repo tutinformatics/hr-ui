fbe23177d8e20ccdb45accddf5586a46cd11fecb
alfrol <alfrol@ttu.ee>
2020-04-29 17:04:23 +0300

    Don not push .env to git.

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -10,6 +10,7 @@ export async function post(req, res) {
         const { userLoginId, currentPassword, currentPasswordVerify } = req.body;
         const { SAPPER_APP_API_URL } = process.env;
 
+        console.log(SAPPER_APP_API_URL);
 
         const response = await fetch(`${SAPPER_APP_API_URL}/api/auth/v1/register`, {
             method: 'POST',
