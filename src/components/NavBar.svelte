ea4845358b5e8d4bfaa8bf4d3bbfcf396d18aa22
alfrol <alfrol@ttu.ee>
2020-05-01 17:13:38 +0300

    Style the navigation bar a little.

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { Nav, Icon } from 'svelte-chota';
+    import { stores } from '@sapper/app';
+
+    const { session } = stores();
+
+    let userLoggedIn = typeof $session.token === 'string';
+    let logo = 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg';
+</script>
+
+<style>
+    a {
+        color: black;
+    }
+</style>
+
+<!-- Seems like we cannot render elements with 'slot' attributes in if blocks,
+because Svelte starts complaining that the element must be a direct child on a component. -->
+{#if !userLoggedIn}
+    <Nav class="navbar">
+        <a slot="left" href="/">Main Page</a>
+
+        <a slot="center" href="/" class="brand">
+            <Icon src={logo} size="2" class="is-rounded navbar__image" />
+        </a>
+
+        <a slot="right" href="/login">Login</a>
+        <a slot="right" href="/register">Register</a>
+    </Nav>
+{:else}
+    <Nav class="navbar">
+        <a slot="left" href="/">Main Page</a>
+
+        <a slot="center" href="/" class="brand">
+            <Icon src={logo} size="2" class="is-rounded navbar__image" />
+        </a>
+        
+        <a slot="right" href="/employees">Employees</a>
+    </Nav>
+{/if}
